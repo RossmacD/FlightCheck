c69c01a23ce78224d8292fd8be15aa8178dd81ef
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-24 21:43:19 +0100

    Made binary executable

--- npm/lib/run.js
+++ npm/lib/run.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 'use strict';
 
 const path = require('path');
 
-module.exports.rgPath = path.join(__dirname, `../bin/rg${process.platform === 'win32' ? '.exe' : ''}`);
+module.exports.rgPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
