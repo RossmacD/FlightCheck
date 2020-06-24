b0bfd76fdebbb48f12ced76d68c004a1c70f8661
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-25 00:51:43 +0100

    Working aside from cursor position moving - could be to do with node encoding

--- npm/lib/run.js
+++ npm/lib/run.js
@@ -2,29 +2,22 @@
 'use strict';
 const path = require('path');
 const {spawn} = require('child_process');
-// const { spawn } = require('process');
 
+// Get the path of the rust binary - generated on install
 const binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
+// Get the commands that are passed into 
 let args = process.argv;
+// The first two arguments are irrelevant - its the original arguments that were passed into the node script
 args=args.slice(2)
-console.log(args)
-args=args.map(arg=>{return arg.charAt(0)!=='-'?process.cwd()+arg:arg})
-console.log(`Sending binary to ${binaryPath} ${args.length!==0?args.join(" "):''}`)
-//Execute the binary with the command line arguments that are passed into node
-// child_process.exec(`${binaryPath} ${args.length!==0?args.join(" "):''}`, (err, stdout, stderr) => {
-//     if (err || stdout || stderr) {
-//         console.log(err || stderr || stdout || "AH yeh");
-//         console.log(stdout || "AH no")
-//     } else { console.log("Nothing") }
-// });
-// ${args.length!==0?args.join(" "):''}
+// console.log(args)
+
+// Where item is not prefixed with `-` assume its a path, prefix the current directory that the command was called from
+args=args.map(arg=>{return arg.charAt(0)!=='-'?process.cwd()+"/"+arg:arg})
+// console.log(`Sending binary to ${binaryPath} ${args.length!==0?args.join(" "):''}`)
+
+// Spawn the binary in a new process and pipe the input / output between the two
 let child = spawn(`${binaryPath}`,args)
-child.stdin.setEncoding('utf-8');
+child.stdin.setEncoding('utf8');
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
 process.stdin.pipe(child.stdin);
-// child.exitCode
-// child.stdin.write("\n");
-
-
-// child_process.spawnSync(`${binaryPath} ${args.length!==0?args.join(" "):''}`,[],{stdio: ['pipe', 'pipe', process.stderr]});
