a55e71743a3bd1411a67c8a6efd13ee722080c94
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-24 22:41:21 +0100

    CLI interop almost working

--- npm/lib/run.js
+++ npm/lib/run.js
@@ -6,11 +6,12 @@ const { stdout } = require('process');
 
 const binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
 let args = process.argv;
-args.shift();
-args=args.forEach(arg=>arg.charAt(0)!=='-'?__dirname+arg:arg)
-console.log(`Sending binary to ${binaryPath} ${args.join(" ")}`)
+args=args.slice(2)
+console.log(args)
+args=args.map(arg=>{return arg.charAt(0)!=='-'?process.cwd()+arg:arg})
+console.log(`Sending binary to ${binaryPath} ${args.length!==0?args.join(" "):''}`)
 //Execute the binary with the command line arguments that are passed into node
-child_process.exec(`${binaryPath} ${args.join(" ")}`, (err, stdout, stderr) => {
+child_process.exec(`${binaryPath} ${args.length!==0?args.join(" "):''}`, (err, stdout, stderr) => {
     if (err || stdout || stderr) {
         console.log(err || stderr || stdout || "AH yeh");
         console.log(stdout || "AH no")
