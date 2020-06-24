03375139e30a9db2bf0453feed1cc313fc12b3dc
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-25 00:38:35 +0100

    Binary actually working

--- npm/lib/install.js
+++ npm/lib/install.js
@@ -14,7 +14,7 @@ if (forceInstall) {
     console.log('--force, ignoring caches');
 }
 
-const VERSION = 'v1.0.5';
+const VERSION = 'v1.0.6';
 const BIN_PATH = path.join(__dirname, '../bin');
 
 let exec = require('child_process').exec;
--- npm/lib/run.js
+++ npm/lib/run.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 const path = require('path');
-const child_process = require('child_process');
-const { stdout } = require('process');
+const {spawn} = require('child_process');
+// const { spawn } = require('process');
 
 const binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
 let args = process.argv;
@@ -11,9 +11,20 @@ console.log(args)
 args=args.map(arg=>{return arg.charAt(0)!=='-'?process.cwd()+arg:arg})
 console.log(`Sending binary to ${binaryPath} ${args.length!==0?args.join(" "):''}`)
 //Execute the binary with the command line arguments that are passed into node
-child_process.exec(`${binaryPath} ${args.length!==0?args.join(" "):''}`, (err, stdout, stderr) => {
-    if (err || stdout || stderr) {
-        console.log(err || stderr || stdout || "AH yeh");
-        console.log(stdout || "AH no")
-    } else { console.log("Nothing") }
-});
+// child_process.exec(`${binaryPath} ${args.length!==0?args.join(" "):''}`, (err, stdout, stderr) => {
+//     if (err || stdout || stderr) {
+//         console.log(err || stderr || stdout || "AH yeh");
+//         console.log(stdout || "AH no")
+//     } else { console.log("Nothing") }
+// });
+// ${args.length!==0?args.join(" "):''}
+let child = spawn(`${binaryPath}`,args)
+child.stdin.setEncoding('utf-8');
+child.stdout.pipe(process.stdout);
+child.stderr.pipe(process.stderr);
+process.stdin.pipe(child.stdin);
+// child.exitCode
+// child.stdin.write("\n");
+
+
+// child_process.spawnSync(`${binaryPath} ${args.length!==0?args.join(" "):''}`,[],{stdio: ['pipe', 'pipe', process.stderr]});
