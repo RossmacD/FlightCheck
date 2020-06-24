9ee4f88a20634909960403264338c51acc35b51c
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-24 22:18:35 +0100

    Added command line processing

--- npm/lib/run.js
+++ npm/lib/run.js
@@ -2,14 +2,17 @@
 'use strict';
 const path = require('path');
 const child_process = require('child_process');
+const { stdout } = require('process');
 
-binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
-
+const binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
+let args = process.argv;
+args.shift();
+args=args.forEach(arg=>arg.charAt(0)!=='-'?__dirname+arg:arg)
+console.log(`Sending binary to ${binaryPath} ${args.join(" ")}`)
 //Execute the binary with the command line arguments that are passed into node
-child_process.exec(`${binaryPath} ${process.argv.join(" ")}`, err => {
-    if (err) {
-        reject(err);
-        return;
-    }
-    resolve();
+child_process.exec(`${binaryPath} ${args.join(" ")}`, (err, stdout, stderr) => {
+    if (err || stdout || stderr) {
+        console.log(err || stderr || stdout || "AH yeh");
+        console.log(stdout || "AH no")
+    } else { console.log("Nothing") }
 });
