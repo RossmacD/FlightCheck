399b73757bd5ae23a880bdb3451d85e74f05783a
Ross MacD <rossmacdonald3411@gmail.com>
2020-06-24 21:53:32 +0100

    Changed run to execute the binary with arguments

--- npm/lib/run.js
+++ npm/lib/run.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-
 const path = require('path');
+const child_process = require('child_process');
 
-module.exports.rgPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
+binaryPath = path.join(__dirname, `../bin/flightcheck${process.platform === 'win32' ? '.exe' : ''}`);
+
+//Execute the binary with the command line arguments that are passed into node
+child_process.exec(`${binaryPath} ${process.argv.join(" ")}`, err => {
+    if (err) {
+        reject(err);
+        return;
+    }
+    resolve();
+});
